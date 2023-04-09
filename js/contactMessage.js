f7853b52136c56c1fd8fad5ac41986e609064fb2
Neha <vneha899@gmail.com>
2023-04-08 23:51:10 -0400

    double click event resolved

--- js/contactMessage.js
+++ js/contactMessage.js
@@ -4,45 +4,40 @@ function userMsg()
     const user_name = document.getElementById('name');
     const user_email = document.getElementById('mail');
     const user_message = document.getElementById('message');
-    const submitButton = document.getElementById('submit-button');
 
-    
-    // Add a click event listener to the submit button
-    submitButton.addEventListener('click', function() 
+    user_name.setCustomValidity('');
+    user_email.setCustomValidity('');
+
+    if ((user_name.value == "")) 
     {
-        user_name.setCustomValidity('');
-        user_email.setCustomValidity('');
+        user_name.setCustomValidity('Please enter your First Name');
+        return;
+    }
+    // validate email and reEmail value
+    if (user_email.value == "") 
+    {
+        user_email.setCustomValidity('Please enter your email.');
+        return;
+    }
+    // Get the user's message from the input field
+    const userData = 
+    {
+        userName : user_name.value,
+        email: user_email.value,
+        msg: user_message.value,
+    };
+    const existingData = JSON.parse(localStorage.getItem('userMessage')) || [];
+    existingData.push(userData);
+    // Convert the user data to a JSON string
+    const userDataJSON = JSON.stringify(existingData);
 
-        if ((user_name.value == "")) 
-        {
-            user_name.setCustomValidity('Please enter your First Name');
-            return;
-        }
-        // validate email and reEmail value
-        if (user_email.value == "") 
-        {
-            user_email.setCustomValidity('Please enter your email.');
-            return;
-        }
-        // Get the user's message from the input field
-        const userData = 
-        {
-            userName : user_name.value,
-            email: user_email.value,
-            msg: user_message.value,
-        };
-        const existingData = JSON.parse(localStorage.getItem('userMessage')) || [];
-        existingData.push(userData);
-        // Convert the user data to a JSON string
-        const userDataJSON = JSON.stringify(existingData);
-
-        // Save the message to local storage
-        localStorage.setItem('userMessage', userDataJSON);
-        alert('Your message sent successfully..');
-        // Clear the input field
-        user_name.value = '';
-        user_email.value = '';
-        user_message.value = '';
-    });
+    // Save the message to local storage
+    localStorage.setItem('userMessage', userDataJSON);
+    alert('Your message sent successfully..');
+    // Clear the input field
+    user_name.value = '';
+    user_email.value = '';
+    user_message.value = '';
+    
 }
 
